846d03879b8e0876c96c173b959f49881e38da4c
htetlinnoo1999 <htetlinnoo19@gmail.com>
2021-08-08 09:20:26 +0630

    state updated

--- src/App.js
+++ src/App.js
@@ -9,13 +9,24 @@ import { useState } from "react";
 
 const queryClient = new QueryClient();
 function App() {
-  const [page, setPage] = useState("bar");
+  const [page, setPage] = useState(() => {
+    return "bar";
+  });
   return (
     <div className="container width-75">
       <div className="d-flex justify-content-between mt-5  mb-7">
-        <Button variant="outline-dark" onClick={() => setPage("bar")}> Bar Chart</Button>
-        <Button variant="outline-dark" onClick={() => setPage("pie")}> Pie Chart</Button>
-        <Button variant="outline-dark" onClick={() => setPage("form")}> Add Data</Button>
+        <Button variant="outline-dark" onClick={() => setPage("bar")}>
+          {" "}
+          Bar Chart
+        </Button>
+        <Button variant="outline-dark" onClick={() => setPage("pie")}>
+          {" "}
+          Pie Chart
+        </Button>
+        <Button variant="outline-dark" onClick={() => setPage("form")}>
+          {" "}
+          Add Data
+        </Button>
       </div>
 
       <QueryClientProvider client={queryClient}>
